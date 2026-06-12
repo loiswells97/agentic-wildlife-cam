b72816b8df664bb018fc139873e2ae4a05f1d91f
Lois Wells <lois.wells@raspberrypi.org>
2026-06-12 17:11:28 +0100

    adding led helpers

--- gpio.py
+++ gpio.py
@@ -1,13 +1,32 @@
-from gpiozero import LED, TonalBuzzer
+from gpiozero import LED, RGBLED, TonalBuzzer
 from time import sleep
 
 def turn_on_led(pin_number):
     led = LED(pin_number)
     led.on()
+    if led.is_lit:
+        return True
+    return False
 
-def turn_of_led(pin_number):
+def turn_off_led(pin_number):
     led = LED(pin_number)
     led.off()
+    if not led.is_lit:
+        return True
+    return False
+
+def blink_led(pin_number, on_time, off_time, n):
+    led = LED(pin_number)
+    led.blink(on_time, off_time, n, background=True)
+    return True
+
+def play_rgb_led_pattern(red_pin_number, green_pin_number, blue_pin_number, pattern):
+    rgb_led = RGBLED(red_pin_number, green_pin_number, blue_pin_number)
+    for colour, duration in pattern:
+        rgb_led.color = colour
+        sleep(duration)
+    rgb_led.off()
+    return True
 
 def play_buzzer_tune(pin_number, tune):
     buzzer = TonalBuzzer(pin_number)
@@ -27,5 +46,5 @@ def play_buzzer_tune(pin_number, tune):
 
     # Turn off completely when finished
     buzzer.stop()
-
+    return True
 # play_buzzer_tune(27, [('C4', 0.5), ('C4', 0.5), ('G4', 0.5), ('G4', 0.5), ('A4', 0.5), ('A4', 0.5), ('G4', 1.0)])
