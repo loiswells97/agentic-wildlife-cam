49d9983816a9fad68090690b38cfefb31b2dffff
Lois Wells <lois.wells@raspberrypi.org>
2026-06-12 15:45:53 +0100

    add buzzer tune helper and env example

--- gpio.py
+++ gpio.py
@@ -1,4 +1,5 @@
 from gpiozero import LED, TonalBuzzer
+from time import sleep
 
 def turn_on_led(pin_number):
     led = LED(pin_number)
@@ -8,5 +9,23 @@ def turn_of_led(pin_number):
     led = LED(pin_number)
     led.off()
 
-def play_buzzer_sound(pin_number, sound):
+def play_buzzer_tune(pin_number, tune):
     buzzer = TonalBuzzer(pin_number)
+    # Define your tune: (Note, Duration in seconds)
+    # Tune should be in the format of a list of tuples, each tuple containing a note and a duration e.g.
+    # tune = [
+    #     ('C4', 0.5), ('C4', 0.5), ('G4', 0.5), ('G4', 0.5),
+    #     ('A4', 0.5), ('A4', 0.5), ('G4', 1.0)
+    # ]
+
+    # Play the tune
+    for note, duration in tune:
+        buzzer.play(note)
+        sleep(duration)
+        buzzer.stop()    # Pause briefly between notes
+        sleep(0.1)
+
+    # Turn off completely when finished
+    buzzer.off()
+
+play_buzzer_tune(17, [('C4', 0.5), ('C4', 0.5), ('G4', 0.5), ('G4', 0.5), ('A4', 0.5), ('A4', 0.5), ('G4', 1.0)])
